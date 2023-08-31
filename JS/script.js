66430f20b8bd14a8d0fb499e15736892acb8767b
Igor Siamykin <sparkisisss@gmail.com>
2023-08-31 22:26:00 +0300

    31-08-23

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,15 @@
 "use strict";
-const numberOfFilms = +prompt('How many of the film you watched?', '');
+let numberOfFilms;
 
-if(numberOfFilms < 10) {
-  alert(`You watched not a lot of films.`);
-} else if(numberOfFilms >= 10 && numberOfFilms <= 30) {
-  alert(`You are a classic watcher.`);
-} else if(numberOfFilms > 30) {
-  alert(`You are a grandmaster.`);
-}else alert(`Error.`);
+function start() {
+  numberOfFilms = +prompt('How many of the film you watched?', '');
 
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('How many of the film you watched?', '');
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -31,4 +32,15 @@ for(let i = 0; i < 2; i++) {
     i--;
   }
 }
+
+function rememberMyFilms
+
+if(numberOfFilms < 10) {
+  alert(`You watched not a lot of films.`);
+} else if(numberOfFilms >= 10 && numberOfFilms <= 30) {
+  alert(`You are a classic watcher.`);
+} else if(numberOfFilms > 30) {
+  alert(`You are a grandmaster.`);
+}else alert(`Error.`);
 console.log(personalMovieDB);
+
