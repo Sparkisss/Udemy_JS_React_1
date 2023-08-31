f2331a68058685f0adde2b7774c3b7859af48a96
Igor Siamykin <sparkisisss@gmail.com>
2023-08-31 23:27:56 +0300

    Practice 3

--- JS/script.js
+++ JS/script.js
@@ -19,28 +19,43 @@ const personalMovieDB = {
   privat: false,
 };
 
-
-for(let i = 0; i < 2; i++) {
-  const lastFilm = prompt('Which film do you watched at last time?', '');
-  const gradeFilm = prompt('How do you grade this film?', '');
-  if(lastFilm != null && gradeFilm != null && lastFilm != '' && gradeFilm != '' && 
-  lastFilm.length < 50){
-    personalMovieDB.movies[lastFilm] = gradeFilm;
-    console.log('done');
-  } else {
-    console.log('error');
-    i--;
+function rememberMyFilms() {
+  for(let i = 0; i < 2; i++) {
+    const lastFilm = prompt('Which film do you watched at last time?', '');
+    const gradeFilm = prompt('How do you grade this film?', '');
+    if(lastFilm != null && gradeFilm != null && lastFilm != '' && gradeFilm != '' && 
+    lastFilm.length < 50){
+      personalMovieDB.movies[lastFilm] = gradeFilm;
+      console.log('done');
+    } else {
+      console.log('error');
+      i--;
+    }
   }
 }
+rememberMyFilms();
 
-function rememberMyFilms
+function detectPersonalLevel() {
+  if(numberOfFilms < 10) {
+    alert(`You watched not a lot of films.`);
+  } else if(numberOfFilms >= 10 && numberOfFilms <= 30) {
+    alert(`You are a classic watcher.`);
+  } else if(numberOfFilms > 30) {
+    alert(`You are a grandmaster.`);
+  }else alert(`Error.`);
+}
+detectPersonalLevel();
 
-if(numberOfFilms < 10) {
-  alert(`You watched not a lot of films.`);
-} else if(numberOfFilms >= 10 && numberOfFilms <= 30) {
-  alert(`You are a classic watcher.`);
-} else if(numberOfFilms > 30) {
-  alert(`You are a grandmaster.`);
-}else alert(`Error.`);
-console.log(personalMovieDB);
+function writeYourGeners() {
+  for(let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Your favorite genre number ${i}?`, '');
+  }
+}
+writeYourGeners();
 
+function showMyDB(hidden) {
+  if(!hidden) {
+    console.log(personalMovieDB);    
+  } 
+}
+showMyDB(personalMovieDB.privat);
